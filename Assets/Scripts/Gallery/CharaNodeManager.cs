d5c32379371929a5e52ed8b9476861a95d477711
m4gnett <39447635+3kanAlpha@users.noreply.github.com>
2021-09-25 20:34:45 +0900

    処理を分割して実行される場所の調整

---
+++ Assets/Scripts/Gallery/CharaNodeManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharaNodeManager : MonoBehaviour
+{
+    private CharacterModel character = null;
+
+    private GameObject galleryManager;
+    private GalleryManager man;
+
+    private GameObject overlay;
+    private OverlayManager overlayManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake() {
+        galleryManager = GameObject.Find("GalleryManager");
+        man = galleryManager.GetComponent<GalleryManager>();
+
+        // GameObject.FindだとinactiveなGameObjectを探せないのでこうなっている
+        var tmp = GameObject.Find("Canvas");
+        overlay = tmp.transform.Find("Overlay").gameObject;
+        overlayManager = overlay.GetComponent<OverlayManager>();
+    }
+
+    // キャラをタップしたときの処理
+    // オーバーレイ画面を開く
+    public void OnClick() {
+        Debug.Log("clicked " + this.character.id);
+
+        overlay.SetActive(true);
+        overlayManager.OpenOverlay(this.character);
+    }
+
+    // setterにキャラアイコン変更の処理を付けている
+    private const string galleryPathBase = "Images/gallery/";
+    private const string lockedPath = galleryPathBase + "face/locked";  // 未解禁キャラクター用
+    public void SetCharacter(CharacterModel model) {
+        this.character = model;
+
+        GameObject faceObject = this.transform.Find("Panel/Face").gameObject;
+        GameObject nameObject = this.transform.Find("Panel/Text").gameObject;
+
+        Image faceImage = faceObject.GetComponent<Image>();
+        Sprite faceSprite = Resources.Load<Sprite>("Images/gallery/face/" + model.id);
+        Sprite lockedSprite = Resources.Load<Sprite>(lockedPath);
+
+        Text nameText = nameObject.GetComponent<Text>();
+
+        if (!man.GetIsUnlocked(model.id)) {
+            faceImage.sprite = lockedSprite;
+            nameText.text = "???";
+        }
+        else {
+            if (faceSprite != null) faceImage.sprite = faceSprite;
+            nameText.text = model.name;
+        }
+    }
+
+    // オーバーレイに表示する
+    public CharacterModel GetCharacter() {
+        return this.character;
+    }
+}
